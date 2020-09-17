ecc336f2057abce892874024950690287329bed7
Mikhail Chekov <chekoff@list.ru>
2020-09-17 16:16:16 +0300

    Updates

---
+++ precache-manifest.236cb52e21b7eb41361fdcfd75a2f451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f002cc1a96305fd11905360e0fbc5c26",
+    "url": "/table-react-app/index.html"
+  },
+  {
+    "revision": "0b3ec09c3aa252c6eee3",
+    "url": "/table-react-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6bfdf3ad2a699b8422c3",
+    "url": "/table-react-app/static/css/main.d364c614.chunk.css"
+  },
+  {
+    "revision": "0b3ec09c3aa252c6eee3",
+    "url": "/table-react-app/static/js/2.0e388f6f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/table-react-app/static/js/2.0e388f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfdf3ad2a699b8422c3",
+    "url": "/table-react-app/static/js/main.e23ea46d.chunk.js"
+  },
+  {
+    "revision": "ec9103488b6b849c0a12",
+    "url": "/table-react-app/static/js/runtime-main.6604cafe.js"
+  }
+]);
